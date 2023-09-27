95399789030e2de62503aa0d22fdbbe27afcb446
MirunaMb <mirunamarcela98@gmail.com>
2023-09-27 16:22:50 +0200

    created file

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Hotel</title>
+        <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+        <!-- CUSTOM CSS -->
+        <link rel="stylesheet" href="./css/style.css">
+
+    </head>
+
+    <body>
+        <div class="wrapper container rounded p-4 mt-5 text-center">
+            <h1>Hi!</h1>
+            <!-- FILTER FORM -->
+            <form action="index.php" method="get">
+                <div class="form-group">
+                    <label for=""> </label>
+                    <input type="checkbox" name="" id="" value="">
+                </div>
+                <button type="Submit" class="btn btn-primary">Filtra</button>
+            </form>
+
+        </div>
+
+
+    </body>
+
+</html>
